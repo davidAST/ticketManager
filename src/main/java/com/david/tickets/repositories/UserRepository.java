6603fdd283a5e59afa1636682d7ef5bfed79fb9e
davidAST <davidad5522@gmail.com>
2026-06-02 16:01:55 +0200

    Configure JPA auditing for User entity

---
+++ src/main/java/com/david/tickets/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.david.tickets.repositories;
+
+import com.david.tickets.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+}
